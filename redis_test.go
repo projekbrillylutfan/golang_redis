5bee878659ac066e0a3a16779e93c1c96637b5e3
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-10-19 10:11:00 +0700

    mantap redis

---
+++ redis_test.go
@@ -0,0 +1,78 @@
+package belajar_golang_redis
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/stretchr/testify/assert"
+)
+
+var client = redis.NewClient(&redis.Options{
+	Addr: "localhost:6379",
+	DB:   0,
+})
+
+func TestConnection(t *testing.T) {
+	assert.NotNil(t, client)
+
+	err := client.Close()
+	assert.Nil(t, err)
+}
+
+var ctx = context.Background()
+
+func TestPing(t *testing.T) {
+	result, err  := client.Ping(ctx).Result()
+	assert.Nil(t, err)
+	assert.Equal(t, "PONG", result)
+}
+
+func TestString(t *testing.T) {
+	client.SetEx(ctx, "name", "Eko Kurniawan", time.Second * 3)
+
+	result, err  := client.Get(ctx, "name").Result()
+	assert.Nil(t, err)
+	assert.Equal(t, "Eko Kurniawan", result)
+
+	time.Sleep(time.Second * 4)
+	result, err = client.Get(ctx, "name").Result()
+	assert.NotNil(t, err)
+}
+
+func TestList(t *testing.T) {
+	client.RPush(ctx, "names", "Eko")
+	client.RPush(ctx, "names", "Kurniawan")
+	client.RPush(ctx, "names", "Khannedy")
+
+	assert.Equal(t, "Eko", client.LPop(ctx, "names").Val())
+	assert.Equal(t, "Kurniawan", client.LPop(ctx, "names").Val())
+	assert.Equal(t, "Khannedy", client.LPop(ctx, "names").Val())
+
+	client.Del(ctx, "names")
+}
+
+func TestSet(t *testing.T) {
+	client.SAdd(ctx, "students", "Eko")
+	client.SAdd(ctx, "students", "Eko")
+	client.SAdd(ctx, "students", "Kurniawan")
+	client.SAdd(ctx, "students", "Kurniawan")
+	client.SAdd(ctx, "students", "Khannedy")
+	client.SAdd(ctx, "students", "Khannedy")
+
+	assert.Equal(t, int64(3), client.SCard(ctx, "students").Val())
+	assert.Equal(t, []string{"Eko", "Kurniawan", "Khannedy"}, client.SMembers(ctx, "students").Val())
+}
+
+func TestSortedSet(t *testing.T) {
+	client.ZAdd(ctx, "scores", redis.Z{Score: 100, Member: "Eko"})
+	client.ZAdd(ctx, "scores", redis.Z{Score: 85, Member: "Budi"})
+	client.ZAdd(ctx, "scores", redis.Z{Score: 95, Member: "Joko"})
+
+	assert.Equal(t, []string{"Budi", "Joko", "Eko"}, client.ZRange(ctx, "scores", 0, -1).Val())
+
+	assert.Equal(t, "Eko", client.ZPopMax(ctx, "scores").Val()[0].Member)
+	assert.Equal(t, "Joko", client.ZPopMax(ctx, "scores").Val()[0].Member)
+	assert.Equal(t, "Budi", client.ZPopMax(ctx, "scores").Val()[0].Member)
+}
